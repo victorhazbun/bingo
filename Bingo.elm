f75f0544171a895d7f610bffa1ee01846f13b980
victor hazbun <victorhazbun87@gmail.com>
2018-03-23 02:43:10 -0500

    Rendering HTML

--- Bingo.elm
+++ Bingo.elm
@@ -1,17 +1,43 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
 
-playerInfoText name gameNumber =
-    playerInfo name gameNumber
-        |> String.toUpper
-        |> Html.text
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "classy" ]
+            [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org" ]
+            [ text "Powered By Elm" ]
+        ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "Nicole" 4
+        , viewFooter
+        ]
 
 
 main =
-    playerInfoText "Mike" 3
+    view
