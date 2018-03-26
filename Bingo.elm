fcd9dbbeafa8416b8f5479312b0446b747ba79d2
victor hazbun <victorhazbun87@gmail.com>
2018-03-26 01:29:00 -0500

    Failures, Maybe?

--- Bingo.elm
+++ Bingo.elm
@@ -16,6 +16,7 @@ type alias Model =
     { name : String
     , gameNumber : Int
     , entries : List Entry
+    , alertMessage : Maybe String
     }
 
 
@@ -32,6 +33,7 @@ initialModel =
     { name = "Mike"
     , gameNumber = 1
     , entries = []
+    , alertMessage = Nothing
     }
 
 
@@ -45,6 +47,7 @@ type Msg
     | Sort
     | NewRandom Int
     | NewEntries (Result Http.Error (List Entry))
+    | CloseAlert
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -63,10 +66,24 @@ update msg model =
 
                 Err error ->
                     let
-                        _ =
-                            Debug.log "Oops!" error
+                        errorMessage =
+                            case error of
+                                Http.NetworkError ->
+                                    "Is the server running?"
+
+                                Http.BadStatus response ->
+                                    (toString response.status)
+
+                                Http.BadPayload message _ ->
+                                    "Decoding Failed: " ++ message
+
+                                _ ->
+                                    (toString error)
                     in
-                        ( model, Cmd.none )
+                        { model | alertMessage = Just errorMessage } ! [ Cmd.none ]
+
+        CloseAlert ->
+            { model | alertMessage = Nothing } ! [ Cmd.none ]
 
         Mark id ->
             let
@@ -195,6 +212,7 @@ view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
         , viewPlayer model.name model.gameNumber
+        , viewAlertMessage model.alertMessage
         , viewEntryList model.entries
         , viewScore (sumMarkedPoints model.entries)
         , div [ class "button-group" ]
@@ -204,6 +222,19 @@ view model =
         , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
+
+
+viewAlertMessage : Maybe String -> Html Msg
+viewAlertMessage alertMessage =
+    case alertMessage of
+        Just message ->
+            div [ class "alert" ]
+                [ span [ class "close", onClick CloseAlert ] [ text "X" ]
+                , text message
+                ]
+
+        Nothing ->
+            text ""
 
 
 init : ( Model, Cmd Msg )
